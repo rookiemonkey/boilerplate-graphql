76851d43ff2d3fb876554b9059a9de817c661175
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-20 18:45:58 +0800

    added logs for starting up the node.js local graphql server

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,5 +2,5 @@ import '@babel/polyfill/noConflict'
 import server from './server'
 
 server.start({ port: process.env.PORT || 3030 }, () => {
-    console.log("NodeJS Graphql API Started")
+    if (!process.env.PORT) { console.log("NodeJS Graphql API Started at http://localhost:3030") }
 })
